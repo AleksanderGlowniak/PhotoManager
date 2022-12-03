f7ec11bba903d03f6de17d491f19401614d16502
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-12-03 14:26:23 +0100

    Add needed requires.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module com.mycompany.photomanager {
     requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
     requires java.desktop;
     requires java.logging;
+    requires jcalendar;
+    requires java.sql;
     exports com.mycompany.photomanager;
 }
