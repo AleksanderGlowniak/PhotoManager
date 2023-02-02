29d08b659e170f2bb9fc8d2f4ca01a5aec4d6cd3
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2023-02-02 19:08:27 +0100

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.mycompany.photomanager {
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires java.desktop;
+    requires java.logging;
+    requires jcalendar;
+    requires java.sql;
+    requires jcommon;
+    requires AbsoluteLayout.RELEASE150;
+    requires jdk.charsets;
+    requires org.jfree.jfreechart;
+    requires java.compiler;
+    exports com.mycompany.photomanager;
+}
